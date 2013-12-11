27d2d95d9b2037d69e6b637d9bf0f910f05d2812
Rie <rienamba@mail.ubc.ca>
2013-12-11 15:45:47 -0800

    learning-wrapper
    It adds Custom Post Type - Learning Wrapper which will allow users to
    easily create a Learning Wrapper like the following URL:
    
    http://digitaltattoo.ubc.ca/connect/connect-feature/

---
+++ js/learning_wrapper_admin.js
@@ -0,0 +1,41 @@
+// Java Document
+jQuery(document).ready(function($) {
+
+        $("select#meta-select-icon").change(function(){
+			if ($('option:selected', this).val() == 'icon-other') {
+                    $(".icon_manual").show();
+            } else {
+							$(".icon_manual").hide();
+
+			}
+           });
+		
+		
+		
+		
+    });	
+/*Make the Post Metadata Field Repeatable*/
+jQuery(document).ready(function($) {
+	$('.metabox_submit').click(function(e) {
+		e.preventDefault();
+		$('#publish').click();
+	});
+	$('#add-row').on('click', function() {
+		var row = $('.empty-row.screen-reader-text').clone(true);
+		row.removeClass('empty-row screen-reader-text');
+		row.insertBefore('#repeatable-fieldset-one>ul:last');
+		return false;
+	});
+	$('.remove-row').on('click', function() {
+		$(this).parents('ul').remove();
+		return false;
+	});
+ 
+	$('#repeatable-fieldset-one').sortable({
+		opacity: 0.6,
+		revert: true,
+		cursor: 'move',
+		handle: '.sort'
+	});
+});
+						
